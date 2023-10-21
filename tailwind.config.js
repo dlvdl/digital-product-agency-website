10e42c6ef3c06b3ebdc396c461e8259262456423
Denys <denisligach98@gmail.com>
2023-10-21 15:38:11 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.{html,js}"],
+
+  theme: {
+    extend: {
+      fontFamily: {
+        main: ["Allerta", "sans-serif"],
+      },
+      fontSize: {
+        sm: "0.875rem",
+        base: ["1rem", "1.6rem"],
+        lg: "1.25rem",
+        xl: "2.5rem",
+      },
+      colors: {
+        blue: "#2639ED",
+        "grey-100": "#E7F0FC",
+        "grey-200": "#565656",
+        "grey-300": "#757575",
+      },
+    },
+  },
+  plugins: [],
+}
